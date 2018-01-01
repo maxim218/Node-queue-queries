4c3fc342b40642f2347788df192ae004625187df
maxim218 <31542700+maxim218@users.noreply.github.com>
2018-01-02 01:09:03 +0300

    Добавление работы с POST зпросами
    Теперь в очередь можно добавлять и POST запросы

--- index.js
+++ index.js
@@ -16,31 +16,63 @@ function deleteSlash(s) {
 const mass = [];
 let freeProcess = true;
 
-function main(request, response) {
+function main(request, response, type) {
     let query = deleteSlash(request.url);
+    console.log("_______________________");
     console.log("Query: " + query);
+    console.log("Type: " + type);
 
     let now = 0;
     let finish = parseInt(query);
 
-    let myInterval = setInterval(() => {
-        now += 1;
-        console.log("Interval " + query);
-        if(now === finish) {
-            clearInterval(myInterval);
-            console.log("Answer: " + "OK_" + query);
-            response.end("OK_" + query);
+    if(type === "GET") {
+        let myInterval = setInterval(() => {
+            now += 1;
+            console.log("Interval " + query);
+            if (now === finish) {
+                clearInterval(myInterval);
+                console.log("Answer: " + "OK_" + query);
+                response.end("OK_" + query);
 
-            freeProcess = true;
-            mass.splice(0,1);
-        }
-    }, 1000);
+                freeProcess = true;
+                mass.splice(0, 1);
+            }
+        }, 1000);
+    }
+
+    if(type === "POST") {
+        request.on('data', function(data) {
+            const valueData = data + "";
+
+            let myInterval = setInterval(() => {
+                now += 1;
+                console.log("Interval " + query + "   Data: " + valueData);
+                if (now === finish) {
+                    clearInterval(myInterval);
+                    console.log("Answer: " + "OK_" + query);
+                    response.end("OK_" + query);
+
+                    freeProcess = true;
+                    mass.splice(0, 1);
+                }
+            }, 1000);
+        });
+    }
 }
 
 app.get('/*', function(request, response) {
     mass.push({
         request: request,
-        response: response
+        response: response,
+        type: "GET"
+    });
+});
+
+app.post('/*', function(request, response) {
+    mass.push({
+        request: request,
+        response: response,
+        type: "POST"
     });
 });
 
@@ -55,7 +87,7 @@ let callingInterval = setInterval(() => {
         if(mass.length > 0) {
             freeProcess = false;
             const obj = mass[0];
-            main(obj.request, obj.response);
+            main(obj.request, obj.response, obj.type);
         }
     }
-}, 50);
+}, 3);
